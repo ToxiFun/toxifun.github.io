9d7d1fea094ccec8971c3c66e86e1bdaa4921b6d
ToxiFun <Tonkobossen@hotmail.com>
2020-05-07 20:05:26 +0200

    Add files via upload

---
+++ HomeGym/firebase.js
@@ -0,0 +1,17 @@
+var config = {
+apiKey: "AIzaSyDITJm2Uiye8VSFturMBOWxY5v85S5F4Zc",
+authDomain: "homegym-19546.firebaseapp.com",
+databaseURL: "https://homegym-19546.firebaseio.com",
+projectId: "homegym-19546",
+storageBucket: "homegym-19546.appspot.com",
+messagingSenderId: "680066508164",
+appId: "1:680066508164:web:243b8b6313c36ace9c29d1",
+measurementId: "G-1CHZC4823T"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+var database = firebase.database();
+if (database) console.log("Database load success");
+else console.log("Database load fail");
+var ref = database.ref("tumblingSkills");
